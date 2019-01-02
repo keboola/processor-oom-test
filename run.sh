3f00d8a5f489c4097a5cd71de088a58962da7c2a
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2019-01-02 16:08:46 +0100

    error handling

--- run.sh
+++ run.sh
@@ -24,17 +24,18 @@ cp -r /tmp/processor-oom-test-01/ /tmp/processor-oom-test-03/
 cp -r /tmp/processor-oom-test-01/ /tmp/processor-oom-test-04/
 cp -r /tmp/processor-oom-test-01/ /tmp/processor-oom-test-05/
 
-set +e
 echo "Starting logging"
 rm -f /tmp/pmap.log && rm -f /tmp/ps.log 
+set +e
 timeout --foreground 1 sh -c './log.sh;:'
 
 echo "Starting stress test"
 cd /tmp
+set +e
 timeout --foreground 1 sh -c 'stress --io 50 --hdd 50 --hdd-bytes 10G --timeout 120s;:'
-set -e
 
 echo "Running 5 containers"
+set -e
 sudo docker run --memory=128m --rm --volume /tmp/processor-oom-test-01/data:/data --volume /tmp/processor-oom-test-01/tmp:/tmp --name processor-oom-test-01 processor-skip-lines & \
 sudo docker run --memory=128m --rm --volume /tmp/processor-oom-test-02/data:/data --volume /tmp/processor-oom-test-02/tmp:/tmp --name processor-oom-test-02 processor-skip-lines & \
 sudo docker run --memory=128m --rm --volume /tmp/processor-oom-test-03/data:/data --volume /tmp/processor-oom-test-03/tmp:/tmp --name processor-oom-test-03 processor-skip-lines & \
