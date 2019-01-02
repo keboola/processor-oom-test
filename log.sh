aba6dc41b516819b8fc56ffe14b380776d8eb05f
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2019-01-02 16:05:18 +0100

    error handling

--- log.sh
+++ log.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-set -e
 timeout --signal 9 130 watch -n1 'ps -C /code/main.php -opid --no-heading | xargs sudo pmap -x >> /tmp/pmap.log & ps -C /code/main.php -opid,size,vsize --no-heading >> /tmp/ps.log' 
