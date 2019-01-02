85956fea60f37a154d0bc3bc7f8aeeee369652da
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2019-01-02 11:33:44 +0100

    larger csv file

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ hexdump -v -e '5/1 "%02x""\n"' /dev/urandom |
   awk -v OFS='\t' '
     NR == 1 { print "foo", "bar", "baz" }
     { print substr($0, 1, 8), substr($0, 9, 2), int(NR * 32768 * rand()) }' |
-  head -n 1000000 > /tmp/processor-oom-test/data/in/files/source.csv
+  head -n 100000000 > /tmp/processor-oom-test/data/in/files/source.csv
 
 echo "Running stress & container"
 cd /tmp
