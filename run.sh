b2f189c6cd92c26e98104d06c4154bf4e846e781
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2019-01-08 14:39:28 +0100

    feat: try to parametrize

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Setting up"
-rm -rf /tmp/processor-oom-test-0*
-mkdir -p /tmp/processor-oom-test-01/tmp
-mkdir -p /tmp/processor-oom-test-01/data/in/files
-mkdir -p /tmp/processor-oom-test-01/data/in/tables
-mkdir -p /tmp/processor-oom-test-01/data/out/files
-mkdir -p /tmp/processor-oom-test-01/data/out/tables
+rm -rf $1/processor-oom-test-0*
+mkdir -p $1/processor-oom-test-01/tmp
+mkdir -p $1/processor-oom-test-01/data/in/files
+mkdir -p $1/processor-oom-test-01/data/in/tables
+mkdir -p $1/processor-oom-test-01/data/out/files
+mkdir -p $1/processor-oom-test-01/data/out/tables
 cp ./config.json /tmp/processor-oom-test-01/data/config.json
 
 echo "Generating CSV"
@@ -15,17 +15,17 @@ hexdump -v -e '5/1 "%02x""\n"' /dev/urandom |
   awk -v OFS='\t' '
     NR == 1 { print "foo", "bar", "baz" }
     { print substr($0, 1, 8), substr($0, 9, 2), int(NR * 32768 * rand()) }' |
-  head -n 100000000 > /tmp/processor-oom-test-01/data/in/files/source.csv
+  head -n $2 > $1/processor-oom-test-01/data/in/files/source.csv
 
 echo "Duplicating"
-cp -r /tmp/processor-oom-test-01/ /tmp/processor-oom-test-02/
-cp -r /tmp/processor-oom-test-01/ /tmp/processor-oom-test-03/
-cp -r /tmp/processor-oom-test-01/ /tmp/processor-oom-test-04/
-cp -r /tmp/processor-oom-test-01/ /tmp/processor-oom-test-05/
+cp -r $1/processor-oom-test-01/ $1/processor-oom-test-02/
+cp -r $1/processor-oom-test-01/ $1/processor-oom-test-03/
+cp -r $1/processor-oom-test-01/ $1/processor-oom-test-04/
+cp -r $1/processor-oom-test-01/ $1/processor-oom-test-05/
 
 echo "Starting stress test"
-cd /tmp
+cd $1
 timeout --foreground 1 sh -c 'stress --io 50 --hdd 50 --hdd-bytes 10G --timeout 120s;:'
 
 echo "Running 5 containers"
-seq -w 1 5 | parallel --will-cite eval sudo docker run --memory=256m --rm --volume /tmp/processor-oom-test-0{}/data:/data --volume /tmp/processor-oom-test-0{}/tmp:/tmp --name processor-oom-test-0{} processor-skip-lines
+seq -w 1 5 | parallel --will-cite eval sudo docker run --memory=256m --rm --volume $1/processor-oom-test-0{}/data:/data --volume $1/processor-oom-test-0{}/tmp:/tmp --name processor-oom-test-0{} processor-skip-lines
